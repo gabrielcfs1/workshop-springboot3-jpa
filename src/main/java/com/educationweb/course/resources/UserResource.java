243e4be35bd0b8ac30faf77ffdffe978cdd46f57
unknown <colombogabriel00@gmail.com>
2023-07-07 20:12:15 -0300

    adding user entity and resource

---
+++ src/main/java/com/educationweb/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.educationweb.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educationweb.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "gabriel", "teste@gmail.com", "9999999", "584983");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
