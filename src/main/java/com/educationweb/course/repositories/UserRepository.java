95688a410c86d2bae67d09d39a7964afd9441fd7
unknown <colombogabriel00@gmail.com>
2023-08-07 20:11:18 -0300

    Service layer, component registration

--- src/main/java/com/educationweb/course/repositories/UserRepository.java
+++ src/main/java/com/educationweb/course/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.educationweb.course.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+
 }
